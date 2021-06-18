bce04af43b284a2055db2c7b332d7f05e24edc59
Dominik Kroliczek <dominik@kroliczek.com>
2021-06-18 12:49:15 +0200

    Update fisher

--- fish/config.fish
+++ fish/config.fish
@@ -20,10 +20,17 @@ alias tf12p 'terraform12 plan'
 # ------ Path
 set PATH /home/kruligh/development/flutter/bin $PATH
 set PATH /home/kruligh/development/bin $PATH
+set PATH /home/kruligh/dotfiles/fish/scripts $PATH
 
+# ------ terminal title
 function tt
     set -l title $argv[1]
     function fish_title --inherit-variable title
         echo "$title"
     end
 end
+
+# ------ aws-bastion-ip
+ function get-fargate-ssh-ip
+   aws ec2 describe-network-interfaces --network-interface-ids (aws ecs describe-tasks --cluster $argv[1] --tasks (aws ecs list-tasks --cluster $argv[1] --query "taskArns" --output text --service-name ssh-server) --query "tasks[].attachments[].details[?name==`networkInterfaceId`].value" --output text) --query   "NetworkInterfaces[].Association.PublicIp" --output text
+ end
--- fish/utils.install.fish
+++ fish/utils.install.fish
@@ -2,9 +2,9 @@
 
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
 
-fisher add fishpkg/fish-prompt-metro
-fisher add edc/bass:master
-fisher add FabioAntunes/fish-nvm
+fisher install fishpkg/fish-prompt-metro
+fisher install edc/bass
+fisher install FabioAntunes/fish-nvm
 
 nvm install 12
 
